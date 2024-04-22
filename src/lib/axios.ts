a0e6309dfe740d9094cd075a554714bf2d3fac82
Joana Maria <joanamariacardoso1601@gmail.com>
2024-04-22 14:13:57 +0100

    config api

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,4 +2,8 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: "https://my-json-server.typicode.com/",
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+  },
 });
