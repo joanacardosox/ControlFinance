074a9088cc3bae2b49a5257cf66dda57aaefffea
Joana Maria <joanamariacardoso1601@gmail.com>
2024-05-03 15:34:51 +0100

    date

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-   baseURL: "https://my-json-server.typicode.com/joanacardosox/ControlFinance/",
+  baseURL: "https://my-json-server.typicode.com/joanacardosox/ControlFinance/",
   headers: {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
